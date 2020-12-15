e3da8aabc87bbfe2fb3be1519f9982d0c14b90ac
Daniil <dan.kim.04lb@gmail.com>
2020-12-16 01:54:48 +0600

    15.12.2020

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -3,13 +3,11 @@ import userPhoto from '../../assets/images/manusericon.png';
 import * as axios from 'axios';
 import s from './FindUsers.module.css';
 class FindUsers extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users?count=4&page=1')
       .then((response) => this.props.setUsers(response.data.items));
   }
-
   render() {
     return (
       <div>
